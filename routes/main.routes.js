be31276cf9b337010a53992b054382446a6fe001
TiagoFernandes2403 <tigasmania@hotmail.com>
2021-05-15 11:01:06 +0100

    rotas15.5

--- routes/main.routes.js
+++ routes/main.routes.js
@@ -4,9 +4,9 @@ const controllerJasmin = require('../controllers/jasmin.controller');
 const controllerAuxJasmin = require('../controllers/jasminAux.controller');
 
 //hubspot
-router.post('/Hubspot/hubspot/', controllerhubspot.createClient);
-router.post('Hubspot/update', controllerhubspot.updateClient);
-router.post('Hubspot/create', controllerhubspot.sendTicket);
+router.post('/Hubspot/hubspot', controllerhubspot.createClient);
+router.post('/Hubspot/update', controllerhubspot.updateClient);
+router.post('/Hubspot/create', controllerhubspot.sendTicket);
 router.get('/Hubspot/getClient/:email', controllerhubspot.getClient);
 
 //jasmin
